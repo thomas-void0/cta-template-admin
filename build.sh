f9d7788bd55be5c2727a1920d6923c3fdbfa5709
newrankyangjingyu <yjy15680489038@163.com>
2023-02-21 12:01:19 +0800

    fix: fix build.sh of execute order

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -eu
 
+TAG=th-template:prodution
+
 printf "==========del legacy images==========\n"
 docker rmi -f $TAG 2> /dev/null
 
 printf "==========start build images==========\n"
 
-TAG=th-template:prodution
 docker build --pull -t $TAG -f Dockerfile .
 
 printf "==========del legacy container==========\n"
