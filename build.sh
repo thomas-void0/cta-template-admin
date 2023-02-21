a001175649ad8b52bb54087ec73f203ec1a368be
newrankyangjingyu <yjy15680489038@163.com>
2023-02-21 11:42:55 +0800

    build: add printf for build.sh file

--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -eu
+
+printf "----------start build images--------\n"
+
 TAG=th-template
 docker build --build-arg NODE_IMAGE_TAG=18.14-alpine -t $TAG -f Dockerfile .
-docker push $TAG
 
+printf "----------del legacy container--------\n"
 docker rm -f th-template 2> /dev/null
+
+printf "----------del legacy images--------\n"
 docker rmi -f $TAG 2> /dev/null
 
+printf "----------run new images--------\n"
 docker run --name th-template -d -p 1314:3000 $TAG
