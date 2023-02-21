56b2730001698917a7e49aeed9f5c54881e9b3fd
newrankyangjingyu <yjy15680489038@163.com>
2023-02-21 11:59:57 +0800

    fix: fix build.sh of execute order

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -eu
 
+printf "==========del legacy images==========\n"
+docker rmi -f $TAG 2> /dev/null
+
 printf "==========start build images==========\n"
 
 TAG=th-template:prodution
-docker build --build-arg NODE_IMAGE_TAG=18.14-alpine -t $TAG -f Dockerfile .
+docker build --pull -t $TAG -f Dockerfile .
 
 printf "==========del legacy container==========\n"
 docker rm -f th-template 2> /dev/null
 
-printf "==========del legacy images==========\n"
-docker rmi -f $TAG 2> /dev/null
-
-printf "==========run new images==========\n"
+printf "==========run new container==========\n"
 docker run --name th-template -d -p 1314:3000 $TAG
